02bd18f6e5d4b1e90e9144148efd2e793ef8705b
Reese <reeselevine@yahoo.com>
2014-11-30 22:49:44 -0800

    Working on editing textfields

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from main.models import MessageBar
 
+class MessageBarAdmin(admin.ModelAdmin):
+
+    fieldsets = [
+        (None, {'fields': ['text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+
+
+admin.site.register(MessageBar)
 # Register your models here.
--- main/models.py
+++ main/models.py
@@ -1,3 +1,12 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class MessageBar(models.Model):
+
+    text = models.TextField()
+
+    def __unicode__(self):
+        return self.text
